ce31618db9343391442199908f0e64c0c31f87e9
Anees Hasainar <aneeshassainar786@gmail.com>
2025-08-31 18:45:31 +0530

    updated  vercel congiguration file

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const dotenv=require("dotenv")
 dotenv.config()
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const PORT = process.env.PORT ? process.env.PORT : 3000;
 const DB_URL = process.env.databaseUrl || 'database_url';
 const HOST = process.env.HOST || 'localhost'
 const SECRET_KEY =process.env.TokenKey
